c5cf279f4e72da694d26402f73fdc6ab0cabed3d
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-29 15:27:06 +0000

    Implement ncurses

--- main.c
+++ main.c
@@ -1,1 +1,12 @@
+#include <stdlib.h>
+#include <ncurses.h>
 
+int main(char argc,char**argv) {
+    initscr();
+    printw("Hello World!");
+
+    refresh();
+    getch();
+    endwin();
+    return 0;
+}
