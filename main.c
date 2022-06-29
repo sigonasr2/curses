083ff9db45ee15a56ea59f84e37d32d5582506a7
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-29 19:16:48 +0000

    Struct stuff
    Co-authored-by: sigonasr2 <sigonasr2@gmail.com>

--- main.c
+++ main.c
@@ -4,8 +4,15 @@
 #include "project/extras.h"
 #include "project/utils/utils.h"
 
+struct data{
+    int val1;
+    int val2;
+    int val3;
+};
+
 int main(int argc,char**argv) {
     int*keyLog=calloc(25,sizeof(int));
+    struct data*dataVals=calloc(3,sizeof(struct data));
     unsigned short currentLogCounter=0;
     unsigned int frameCount = 0;
     int rows,cols;
@@ -21,7 +28,20 @@ int main(int argc,char**argv) {
     nodelay(stdscr,TRUE);
     noecho();
 
+    dataVals[0].val1=14;
+    dataVals[0].val2=16;
+    dataVals[0].val3=22;
+    dataVals[1].val1=39;
+    dataVals[1].val2=7;
+    dataVals[1].val3=64;
+    dataVals[2].val1=42;
+    dataVals[2].val2=14;
+    dataVals[2].val3=69;
 
+
+    mvprintw(2,7,"%d %d %d",dataVals[0].val1,dataVals[0].val2,dataVals[0].val3);
+    mvprintw(3,7,"%d %d %d",dataVals[1].val1,dataVals[1].val2,dataVals[1].val3);
+    mvprintw(4,7,"%d %d %d",dataVals[2].val1,dataVals[2].val2,dataVals[2].val3);
     refresh();
     getmaxyx(stdscr,rows,cols);
     int ch=ERR;
@@ -42,6 +62,7 @@ int main(int argc,char**argv) {
         }
     }
     free(keyLog);
+    free(dataVals);
     endwin();
     return 0;
 }
