b2e48d94300a997fb1e56c8cb2b4fc22318e74fc
sigonasr2, Sig, Sigo <sigonasr2@gmail.com>
2022-06-29 15:41:52 +0000

    Learn basic curses functions

--- main.c
+++ main.c
@@ -2,21 +2,24 @@
 #include <ncurses.h>
 
 int main(char argc,char**argv) {
+    int rows,cols;
+
     initscr();
+    getmaxyx(stdscr,rows,cols);
     raw();
     keypad(stdscr,TRUE);
     noecho();
 
-    printw("Type any character:");
-    int ch=getch();
+    addch(ACS_BULLET);
+    addch(ACS_BOARD);
+    addch(ACS_CKBOARD);
+    addch(ACS_BBSS);
+    addch(ACS_BLOCK);
+    addch(ACS_BSBS);
+    addch(ACS_BTEE);
+    mvaddch(5,17,ACS_DARROW);
 
-    if (ch==KEY_F(1)) {
-        printw("F1 Key pressed!");
-    } else {
-        attron(A_BOLD);
-        printw("%c",ch);
-        attroff(A_BOLD);
-    }
+    mvprintw(4,7,"There are %dx%d squares.",rows,cols);
 
     refresh();
     getch();
